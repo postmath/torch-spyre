aadb057876325ebe597515760f819cea232908ca
Erik Postma <epostma@openteams.com>
2026-04-21 16:01:37 -0400

    feat: add four more patterns
    Add four more patterns that are a bit less trivial. Each is tested twice,
    once to test that the proposed allocation is valid, and once (xfail) to
    test that the current implementation matches the proposed allocation.
    
    I also removed a few remaining references to the one global variable I
    had initially introduced, I hadn't removed all places where it was used.
    
    Signed-off-by: Erik Postma <epostma@openteams.com>

--- tests/inductor/test_scratchpad_patterns.py
+++ tests/inductor/test_scratchpad_patterns.py
@@ -62,12 +62,12 @@ class InstrumentedAllocator(ScratchPadAllocator):
         for tensor_name in op.inputs:
             result[tensor_name] = {
                 "is_input": True,
-                "size": all_buffers[tensor_name].size,
+                "size": op._buffer_registry[tensor_name].size,
             }
         for tensor_name in op.outputs:
             result[tensor_name] = {
                 "is_input": False,
-                "size": all_buffers[tensor_name].size,
+                "size": op._buffer_registry[tensor_name].size,
             }
         return result
 
@@ -104,7 +104,7 @@ class TestExamplePattern(TestCase):
                 for j in range(len(sorted_allocations) - 1):
                     buffer_name_j, addr_j = sorted_allocations[j]
                     buffer_name_next, addr_next = sorted_allocations[j + 1]
-                    size_j = all_buffers[buffer_name_j].size
+                    size_j = op._buffer_registry[buffer_name_j].size
                     self.assertLessEqual(
                         addr_j + size_j,
                         addr_next,
@@ -113,12 +113,12 @@ class TestExamplePattern(TestCase):
 
                 self.assertLessEqual(
                     sorted_allocations[-1][1]
-                    + all_buffers[sorted_allocations[-1][0]].size,
+                    + op._buffer_registry[sorted_allocations[-1][0]].size,
                     AVAILABLE_LX_SIZE,
                     f"Buffer {sorted_allocations[-1][0]} exceeds scratch pad size during operation {op.name}",
                 )
 
-    def hbm_usage_test(
+    def hbm_usage_for_good_allocation(
         self, allocation: AllocationResult, operations: list[Operation]
     ) -> int:
         hbm_usage = 0
@@ -126,16 +126,19 @@ class TestExamplePattern(TestCase):
             for buffer_name in op.inputs:
                 if i == 0 or buffer_name not in allocation[i - 1]:
                     # This buffer is not allocated in the scratch pad before this operation, so it must be loaded from HBM.
-                    hbm_usage += all_buffers[buffer_name].size
+                    hbm_usage += op._buffer_registry[buffer_name].size
             for buffer_name in op.outputs:
                 if i == len(operations) - 1 or buffer_name not in allocation[i + 1]:
                     # This buffer is not allocated in the scratch pad after this operation, so it must be stored to HBM.
-                    hbm_usage += all_buffers[buffer_name].size
+                    hbm_usage += op._buffer_registry[buffer_name].size
         return hbm_usage
 
-    def hbm_usage_current_run(
+    def hbm_usage_for_actual_run(
         self, operations: list[Operation], alloc: InstrumentedAllocator
     ) -> int:
+        if not operations:
+            return 0
+
         hbm_usage = 0
 
         # Count all usage for buffers not allocated in the scratchpad.
@@ -143,25 +146,22 @@ class TestExamplePattern(TestCase):
             for buffer_name in op.inputs:
                 if i == 0 or buffer_name not in alloc.allocations:
                     # This buffer is not allocated in the scratch pad before this operation, so it must be loaded from HBM.
-                    hbm_usage += all_buffers[buffer_name].size
+                    hbm_usage += op._buffer_registry[buffer_name].size
             for buffer_name in op.outputs:
                 if i == len(operations) - 1 or buffer_name not in alloc.allocations:
                     # This buffer is not allocated in the scratch pad after this operation, so it must be stored to HBM.
-                    hbm_usage += all_buffers[buffer_name].size
+                    hbm_usage += op._buffer_registry[buffer_name].size
 
         # All buffers allocated in the scratchpad are counted only once each.
         for buffer_name in alloc.allocations:
-            hbm_usage += all_buffers[buffer_name].size
+            hbm_usage += operations[0]._buffer_registry[buffer_name].size
 
         return hbm_usage
 
-    def run_test_case(self, test_case: AllocationTestCase):
-        global all_buffers
-        all_buffers = test_case.buffers
-
-        # Assert that the "good allocation" is indeed valid
+    def verify_test_case(self, test_case: AllocationTestCase):
         self.assertIsValidAllocation(test_case.good_allocation, test_case.operations)
 
+    def run_test_case(self, test_case: AllocationTestCase):
         alloc = InstrumentedAllocator()
 
         scratchpad_planning(test_case.operations, alloc)
@@ -171,8 +171,8 @@ class TestExamplePattern(TestCase):
 
         # Verify that the currently implemented allocation is at least as good as the "good
         # allocation" in terms of HBM usage.
-        current_hbm_usage = self.hbm_usage_current_run(test_case.operations, alloc)
-        good_hbm_usage = self.hbm_usage_test(
+        current_hbm_usage = self.hbm_usage_for_actual_run(test_case.operations, alloc)
+        good_hbm_usage = self.hbm_usage_for_good_allocation(
             test_case.good_allocation, test_case.operations
         )
 
@@ -182,8 +182,11 @@ class TestExamplePattern(TestCase):
             f"Current allocation uses more HBM ({current_hbm_usage} bytes) than the good allocation ({good_hbm_usage} bytes). ",
         )
 
-    @expectedFailure
-    def test_simple_fragmentation_pattern(self):
+    def make_simple_fragmentation_pattern_test_case(self) -> AllocationTestCase:
+        """Allocate two buffers A and B that are each a quarter of the scratchpad size, where the
+        first buffer can be freed after the second operation. Then allocate a third buffer that is
+        half the scratchpad size. This can only fit if B was allocated at the start or end of the
+        scratchpad, leaving a contiguous region for C."""
         quarter_scratchpad_size = 1 << 19  # 512KB
         buffers = {}
         buffers["A"] = Buffer("A", quarter_scratchpad_size)
@@ -196,7 +199,7 @@ class TestExamplePattern(TestCase):
         op2 = Operation("op2", inputs=["B"], outputs=["A"], _buffer_registry=buffers)
         op3 = Operation("op3", inputs=["B"], outputs=["C"], _buffer_registry=buffers)
 
-        testcase = AllocationTestCase(
+        return AllocationTestCase(
             buffers,
             [op1, op2, op3],
             good_allocation=[
@@ -207,7 +210,247 @@ class TestExamplePattern(TestCase):
                 {"B": 0, "C": quarter_scratchpad_size},
             ],
         )
-        self.run_test_case(testcase)
+
+    def test_verify_simple_fragmentation_pattern(self):
+        self.verify_test_case(self.make_simple_fragmentation_pattern_test_case())
+
+    @expectedFailure
+    def test_simple_fragmentation_pattern(self):
+        self.run_test_case(self.make_simple_fragmentation_pattern_test_case())
+
+    def make_staircase_pattern_test_case(self) -> AllocationTestCase:
+        """Allocate N*2 buffers of sizes k, k, 2*k, 2*k, 3*k, 3*k, ..., N*k, N*k. After an
+        even-numbered buffer is allocated, free the previous odd-numbered buffer. This creates a
+        "staircase" pattern of allocations that can only be fit if the allocator is smart about
+        fragmentation. In that case, the maximum scratchpad usage is
+        k + 2*k + ... + N*k + N*k = k * N * (N + 1) / 2 + N * k = k * N * (N + 3) / 2, so we choose
+        k such that this is just less than the available scratchpad size.
+
+        The greedy allocator will always allocate the next buffer just after all other buffers,
+        because no gap is big enough for the current size. So it uses
+        2 * (k + 2*k + ... + N*k) = k * N * (N + 1) or roughly 2/3 times more."""
+        N = 7
+        k = (2 * AVAILABLE_LX_SIZE) // (N * (N + 3))
+        k = (k // 128) * 128  # round down to a multiple of the stick size
+
+        # This only works if the greedy allocator uses more than fits in the scratchpad, so we
+        # assert that here.
+        self.assertGreater(k * N * (N + 1), AVAILABLE_LX_SIZE)
+
+        buffers = {
+            f"{letter}{i}": Buffer(f"{letter}{i}", i * k)
+            for i in range(1, N + 1)
+            for letter in ["A", "B"]
+        }
+
+        def op_pair(i: int) -> tuple[Operation, Operation]:
+            return (
+                Operation(
+                    f"op{i}_0",
+                    inputs=[f"A{i}"],
+                    outputs=[f"B{i}"],
+                    _buffer_registry=buffers,
+                ),
+                Operation(
+                    f"op{i}_1",
+                    inputs=[f"B{i}"],
+                    outputs=[f"A{i}"],
+                    _buffer_registry=buffers,
+                ),
+            )
+
+        ops = [op for i in range(1, N + 1) for op in op_pair(i)] + [
+            Operation(
+                "op_final",
+                inputs=[f"B{i}" for i in range(1, N + 1)],
+                outputs=[],
+                _buffer_registry=buffers,
+            )
+        ]
+
+        def good_allocation_pair(i: int) -> tuple[dict[str, int], dict[str, int]]:
+            # Allocate A{i} at 0 and B{j} for j <= i at N*k, (N+1)*k, (N+3)*k, (N+6)*k, ...,
+            # (N+i*(i-1)/2)*k.
+            alloc = {f"B{j}": (N + j * (j - 1) // 2) * k for j in range(1, i + 1)}
+            alloc[f"A{i}"] = 0
+            return (alloc, alloc)
+
+        good_allocations = [
+            alloc for i in range(1, N + 1) for alloc in good_allocation_pair(i)
+        ]
+        good_allocations.append(
+            {f"B{i}": (N + i * (i - 1) // 2) * k for i in range(1, N + 1)}
+        )
+
+        testcase = AllocationTestCase(buffers, ops, good_allocation=good_allocations)
+        return testcase
+
+    def test_verify_staircase_pattern(self):
+        self.verify_test_case(self.make_staircase_pattern_test_case())
+
+    @expectedFailure
+    def test_staircase_pattern(self):
+        self.run_test_case(self.make_staircase_pattern_test_case())
+
+    def make_downward_staircase_pattern_test_case(self) -> AllocationTestCase:
+        """Allocate 1+N*2 buffers of sizes k, N*k, N*k, (N-1)*k, (N-1)*k, ..., 2*k, 2*k, k, k.
+        After an odd-numbered buffer (>1) is allocated, free the previous even-numbered buffer.
+        This creates an easier "staircase" pattern of allocations than in
+        `make_staircase_pattern_test_case`. Still, the greedy allocator will prefer to allocate
+        buffers at the end if it can't allocate them at address 0. So we first allocate one small
+        buffer at the start which will block address 0. In the optimal case, the maximum scratchpad
+        usage is k + N*k + (N-1)*k + ... + 2*k + k + k = k * (4 + N * (N + 1)) / 2, so we choose k
+        such that this is just less than the available scratchpad size.
+
+        The greedy allocator will always allocate the next buffer just after all other buffers,
+        up until the point where it fills up the cache and starts looking for gaps. The total usage
+        is less clear to analyze."""
+        N = 5
+        k = (2 * AVAILABLE_LX_SIZE) // (4 + N * (N + 1))
+        k = (k // 128) * 128  # round down to a multiple of the stick size
+
+        buffers = {
+            f"{letter}{i}": Buffer(f"{letter}{i}", (N + 1 - i) * k)
+            for i in range(1, N + 1)
+            for letter in ["A", "B"]
+        }
+        buffers["Z"] = Buffer("Z", k)
+
+        def op_pair(i: int) -> tuple[Operation, Operation]:
+            return (
+                Operation(
+                    f"op{i}_0",
+                    inputs=[f"A{i}"],
+                    outputs=[f"B{i}"],
+                    _buffer_registry=buffers,
+                ),
+                Operation(
+                    f"op{i}_1",
+                    inputs=[f"B{i}"],
+                    outputs=[f"A{i}"],
+                    _buffer_registry=buffers,
+                ),
+            )
+
+        ops = (
+            [Operation("op_start", inputs=["Z"], outputs=[], _buffer_registry=buffers)]
+            + [op for i in range(1, N + 1) for op in op_pair(i)]
+            + [
+                Operation(
+                    "op_final",
+                    inputs=[f"B{i}" for i in range(1, N + 1)],
+                    outputs=["Z"],
+                    _buffer_registry=buffers,
+                )
+            ]
+        )
+
+        def good_allocation_pair(i: int) -> tuple[dict[str, int], dict[str, int]]:
+            # Allocate Z at 0, A{i} at k, and B{j} for j <= i as follows: B{N} at 2*k, B{N-1} at
+            # 3*k, B{N-2} at 5*k, B{N-3} at 8*k, ..., B{N-j} at (j*(j+1)/2 + 2)*k. The gap
+            # between A{i} and B{i} = B{N+1-(N+1-i)} is (N+1-i)*(N+2-i)/2*k, which is big enough
+            # for A{i+1} of size (N-i)*k.
+            alloc = {f"B{N - j}": (j * (j + 1) // 2 + 2) * k for j in range(N - i, N)}
+            alloc["Z"] = 0
+            alloc[f"A{i}"] = k
+            return (alloc, alloc)
+
+        good_allocations = [{"Z": 0}]
+        good_allocations.extend(
+            [alloc for i in range(1, N + 1) for alloc in good_allocation_pair(i)]
+        )
+        last_allocation = good_allocation_pair(N)[0]
+        del last_allocation[f"A{N}"]  # A{N} is not needed for the final op
+        good_allocations.append(last_allocation)
+
+        testcase = AllocationTestCase(buffers, ops, good_allocation=good_allocations)
+        return testcase
+
+    def test_verify_downward_staircase_pattern(self):
+        self.verify_test_case(self.make_downward_staircase_pattern_test_case())
+
+    @expectedFailure
+    def test_downward_staircase_pattern(self):
+        self.run_test_case(self.make_downward_staircase_pattern_test_case())
+
+    def make_simple_eviction_pattern_test_case(self) -> AllocationTestCase:
+        """This pattern requires allocating a buffer, evicting it, and then reallocating it later.
+
+        We use two buffers A and B that are each exactly the available LX size. We have six
+        operations. The first two use A, the next two use B, and the last two use A again. Optimal
+        use would allocate A and B for two ops each at alternate times."""
+        buffers = {
+            "A": Buffer("A", AVAILABLE_LX_SIZE),
+            "B": Buffer("B", AVAILABLE_LX_SIZE),
+        }
+        ops = [
+            Operation("op1", inputs=["A"], outputs=[], _buffer_registry=buffers),
+            Operation("op2", inputs=["A"], outputs=[], _buffer_registry=buffers),
+            Operation("op3", inputs=["B"], outputs=[], _buffer_registry=buffers),
+            Operation("op4", inputs=["B"], outputs=[], _buffer_registry=buffers),
+            Operation("op5", inputs=["A"], outputs=[], _buffer_registry=buffers),
+            Operation("op6", inputs=["A"], outputs=[], _buffer_registry=buffers),
+        ]
+
+        good_allocation = [{"A": 0}] * 2 + [{"B": 0}] * 2 + [{"A": 0}] * 2
+
+        testcase = AllocationTestCase(buffers, ops, good_allocation=good_allocation)
+        return testcase
+
+    def test_verify_simple_eviction_pattern(self):
+        self.verify_test_case(self.make_simple_eviction_pattern_test_case())
+
+    @expectedFailure
+    def test_simple_eviction_pattern(self):
+        self.run_test_case(self.make_simple_eviction_pattern_test_case())
+
+    def make_eviction_reallocation_pattern_test_case(self) -> AllocationTestCase:
+        """This pattern requires allocating a buffer, evicting it, and then reallocating it later
+        at a different address to achieve optimality.
+
+        We use four buffers total: A0, A1, A2 of size 1/3 the available size, and B of size twice
+        that. We first ensure that A0, A1, and A2 must be allocated together, then A0 and B, then
+        A1 and B, and finally A2 and B. Because B can fit only with one of the A buffers at the top
+        or the bottom, whichever one was allocated in the middle must be moved.
+
+        We ensure that any set is allocated together in an optimal allocation by using four ops
+        in a row that use them all as input. This means that, whatever was in the scratchpad before
+        and whatever is in it after, we can complete that phase with one full scratchpad worth of
+        HBM transfers. On the other hand, if not everything is allocated on the scratchpad, then we
+        have to stream at least one buffer four times, which entails at least 4/3 of the scratchpad
+        size in HBM transfers."""
+        A_size = AVAILABLE_LX_SIZE // 3
+        A_size = (A_size // 128) * 128  # round down to a multiple of the stick size
+        B_size = 2 * A_size
+
+        # This will work if 4 * A_size > AVAILABLE_LX_SIZE.
+        self.assertGreater(4 * A_size, AVAILABLE_LX_SIZE)
+
+        buffers = {f"A{i}": Buffer(f"A{i}", A_size) for i in range(3)}
+        buffers["B"] = Buffer("B", B_size)
+
+        pattern = [["A0", "A1", "A2"], ["A0", "B"], ["A1", "B"], ["A2", "B"]]
+        ops = [
+            Operation(f"op{i}_{j}", inputs=group, outputs=[], _buffer_registry=buffers)
+            for i, group in enumerate(pattern)
+            for j in range(4)
+        ]
+
+        good_allocations = (
+            [{"A0": 0, "A1": A_size, "A2": 2 * A_size}] * 4
+            + [{"A0": 0, "B": A_size}] * 4
+            + [{"A1": 0, "B": A_size}] * 4
+            + [{"A2": 0, "B": A_size}] * 4
+        )
+        testcase = AllocationTestCase(buffers, ops, good_allocation=good_allocations)
+        return testcase
+
+    def test_verify_eviction_reallocation_pattern(self):
+        self.verify_test_case(self.make_eviction_reallocation_pattern_test_case())
+
+    @expectedFailure
+    def test_eviction_reallocation_pattern(self):
+        self.run_test_case(self.make_eviction_reallocation_pattern_test_case())
 
 
 if __name__ == "__main__":
