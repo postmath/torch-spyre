b0031c4c963fee5cd16613ab0b9d8c195fc48b2a
Isuru Fernando <ifernando@openteams.com>
2026-06-18 09:07:14 +0000

    Add more tests
    Signed-off-by: Isuru Fernando <ifernando@openteams.com>

--- tests/inductor/test_scratchpad_solver.py
+++ tests/inductor/test_scratchpad_solver.py
@@ -16,6 +16,7 @@
 
 import unittest
 from unittest import TestCase
+
 from torch_spyre._inductor.scratchpad.plan_solver import (
     MemoryPlanSolver,
     CoreDivision,
@@ -40,6 +41,9 @@ from torch_spyre._inductor.scratchpad.firstfit_bestfit_solver import (
     FirstFitLayoutSolver,
     _assert_in_place_relationships,
     _topological_sort,
+)
+from torch_spyre._inductor.scratchpad.imanishi_xu import (
+    ImanishiXuLayoutSolver,
 )
 
 LARGE_SIZE = 512
@@ -155,7 +159,7 @@ class BaseLayoutSolverTests:
             self.make_buffer("buffer1", 3, [0, 1]),
             self.make_buffer("buffer2", 4, [0, 1]),
         ]
-        self.verify_layout(buffers, [0, None, None], alignment=ALIGNMENT)
+        self.verify_layout(buffers, {(0, None, None), (None, None, 0)}, alignment=ALIGNMENT)
 
     def test_alignment_enforced(self):
         # Each buffer is placed at the next alignment boundary.
@@ -833,6 +837,21 @@ class TestCpSatUnallocatedReads(TestCase):
 class TestGreedyLayoutSolver(BaseLayoutSolverTests, TestCase):
     solver_class = GreedyLayoutSolver
 
+
+class TestImanishiXuLayoutSolver(ScoreOrderingTests, BaseLayoutSolverTests, TestCase):
+    solver_class = ImanishiXuLayoutSolver
+    
+    def test_largest_buffer_evicted_when_full(self):
+        # unlike other solvers, imanishi xu finds the optimal solution
+        self.verify_layout(
+            [
+                LifetimeBoundBuffer("a", 10, [0, 3]),
+                LifetimeBoundBuffer("b", 20, [0, 3]),
+                LifetimeBoundBuffer("c", 30, [0, 3]),
+            ],
+            [None, 0, 20],
+            size=50,
+        )
 
 class TestTopologicalSort(TestCase):
     """Tests for the Kahn's-algorithm sort that orders in-place chains.
